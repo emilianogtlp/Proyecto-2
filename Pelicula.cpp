95607c25a7c82453ca0d6c57aace1f950749f959
emilianogtlp <emilianogtlp@gmail.com>
2020-04-12 12:03:27 -0500

    Modificacion pelicula .cpp y .h

--- Pelicula.cpp
+++ Pelicula.cpp
@@ -1,1 +1,12 @@
 #include "Pelicula.h"
+
+Pelicula::Pelicula(int numpeli, int a, int dur, int cantia, std::string titu, std::string gen)
+{
+	NumPeli = numpeli;
+	anio = a;
+	duracion = dur;
+	cantActores = cantia;
+	titulo = titu;
+	genero = gen;
+	listaActores[cantia];
+}
--- Pelicula.h
+++ Pelicula.h
@@ -3,11 +3,12 @@
 class Pelicula
 {
 public:
-
+	Pelicula();
+	Pelicula(int numpeli, int a, int dur, int cantia, std::string titu, std::string gen);
 
 private:
 	int NumPeli, anio, duracion, cantActores;
 	std::string titulo, genero;
-	Actor listaActores[10];
+	Actor listaActores[];
 };
 
